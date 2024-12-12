7104ddcda2e17e00c915703edc83af897ee99141
Rashad Edwards <rashadedwards@macbook.local>
2024-12-12 00:23:55 -0500

    fixed navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,16 +10,16 @@ function NavBar() {
       {/* Top Bar */}
       <div className="flex justify-between items-center px-4 py-3">
         {/* Logo */}
-        <div className="text-2xl text-slate-600 tracking-widest font-sans font-extralight">
+        <div className="text-2xl text-slate-600 tracking-widest font-sans font-bold">
           SC
         </div>
 
         {/* Hamburger Icon */}
         <button
-          className="block lg:hidden text-slate-700 focus:outline-none"
+          className="block lg:hidden text-slate-700 focus:outline-none "
           onClick={toggleMenu}
         >
-          <div className="space-y-2">
+          <div className="space-y-2  font-extrabold ">
             <span className="block w-6 h-0.5 bg-slate-700"></span>
             <span className="block w-6 h-0.5 bg-slate-700"></span>
             <span className="block w-6 h-0.5 bg-slate-700"></span>
@@ -30,11 +30,11 @@ function NavBar() {
       {/* Drop-Down Menu */}
       <div
         className={`${
-          menuOpen ? "max-h-96" : "max-h-0"
-        } overflow-hidden transition-all duration-500 bg-slate-700 lg:max-h-full lg:flex lg:items-center lg:justify-between `}
+          menuOpen ? "translate-x-0" : "-translate-x-full"
+        } fixed inset-y-0 left-0 w-64 mt-14 h-72 bg-slate-700 text-white transition-transform duration-500 lg:relative lg:translate-x-0 lg:flex lg:items-center lg:justify-between pt-8`}
       >
-        <ul className="flex flex-col lg:flex-row lg:items-center gap-6 p-4 lg:p-0 text-white lg:text-gray-700 ">
-          <li className="hover:text-blue-400  lg:hover:text-emerald-400 cursor-pointer">
+        <ul className="flex flex-col lg:flex-row lg:items-center gap-6 p-4 lg:p-0 tracking-widest font-sans font-thin">
+          <li className="hover:text-blue-400 lg:hover:text-emerald-400 cursor-pointer">
             Weather
           </li>
           <li className="hover:text-blue-300 lg:hover:text-emerald-400 cursor-pointer">
